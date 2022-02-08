46193576ef6e2d08fa4fa87779e1316df73ef1ce
Fadhlurrohman Habibi <fadhlurrohmanhabibi46@gmail.com>
2022-02-09 00:17:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kamus/database/DatabaseContract.java
@@ -0,0 +1,18 @@
+package com.example.kamus.database;
+
+
+import android.provider.BaseColumns;
+
+
+public class DatabaseContract {
+
+    static String TABLE_ENGLISH = "tb_inggris";
+    static String TABLE_INDONESIA = "tb_indonesia";
+
+    static final class KamusColumns implements BaseColumns {
+
+        static String KATA = "kata";
+        static String DESKRIPSI = "deskripsi";
+
+    }
+}
